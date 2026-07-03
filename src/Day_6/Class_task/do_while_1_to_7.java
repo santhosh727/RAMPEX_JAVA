5c0dfdd526c6fc01288c017e6d118f376c957b8e
santhosh <santhosh15032007@gmail.com>
2026-07-03 23:46:23 +0530

    example program using do while loop

---
+++ src/Day_6/Class_task/do_while_1_to_7.java
@@ -0,0 +1,11 @@
+package Day_6.Class_task;
+
+public class do_while_1_to_7 {
+    public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.println(i++);
+        }
+        while(i<=7);
+    }
+}
