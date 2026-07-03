66506c15028ca4aa17e16969c62bb2926f1cfed0
santhosh <santhosh15032007@gmail.com>
2026-07-03 19:19:53 +0530

    PROGRAM TO PRINT MY  DETAILS

---
+++ src/Day_1/Class_Task/DETAILS.java
@@ -0,0 +1,12 @@
+package Day_1.Class_Task;
+
+public class DETAILS {
+    public static void main(String[] args)
+    {
+        char initial = 'S';
+        String name = "Santhosh";
+        int age =19;
+        double cgpa = 8.4;
+        System.out.println(" My name is " + name +" ." + initial + " of age  " + age + " with the cgpa of " + cgpa);
+    }
+}
