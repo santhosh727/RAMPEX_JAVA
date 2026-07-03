c399041f3560e45008b89c7ae1f80fb55468bf4a
santhosh <santhosh15032007@gmail.com>
2026-07-03 23:45:24 +0530

    example program using break statement

---
+++ src/Day_6/Class_task/break_example.java
@@ -0,0 +1,12 @@
+package Day_6.Class_task;
+
+public class break_example {
+    public static void main(String[] args){
+        for(int i=1;i<=5;i++){
+            if(i==3){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}
