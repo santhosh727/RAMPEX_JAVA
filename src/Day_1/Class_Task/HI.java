f425ce7ae15edf91928408b4e1b6bb3bbe394048
santhosh <santhosh15032007@gmail.com>
2026-07-03 19:20:09 +0530

    PROGRAM TO PRINT HI

---
+++ src/Day_1/Class_Task/HI.java
@@ -0,0 +1,9 @@
+package Day_1.Class_Task;
+
+public class HI {
+        public static void main(String[] args)       // this is the function
+        {
+            System.out.println("HI");
+        }
+    }
+
