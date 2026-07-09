df6152e543aeee95d60efdca0b90d9615d6ae4e1
santhosh <santhosh15032007@gmail.com>
2026-07-09 10:15:39 +0530

    sample program for seating arrangement

---
+++ src/Day_11/home_task/seating_arrangement.java
@@ -0,0 +1,14 @@
+package Day_11.home_task;
+
+public class seating_arrangement {
+    public static void main(String[] args){
+        int R;
+        int S;
+        for(int i=1;i<6;i++){
+            for (int j=1;j<=6;j++){
+                System.out.print(" R"+i+"C"+j );
+            }
+            System.out.println();
+        }
+    }
+}
